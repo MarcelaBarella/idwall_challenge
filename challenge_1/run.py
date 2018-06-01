2e3016c91f7cd5f94247d4dc4f1ff9eb501f5ce9
Marcela Barella Silva <marcela.barella@hotmail.com>
2018-05-31 22:10:36 -0300

    Implements script that orchestrate solution of first challenge. It should be refactored

--- challenge_1/run.py
+++ challenge_1/run.py
@@ -16,3 +16,17 @@ if not arguments.output:
     input_name = '.'.join(arguments.input.name.split('.')[:-1])
     output_name = input_name + '_output.' + input_extension
     arguments.output = open(output_name, 'w')
+
+
+input_text = arguments.input.read().replace('\r\n', ' ').replace('\n', ' ')
+arguments.input.close()
+
+output_lines = LineBreaker.to_lines(input_text)
+
+if(arguments.action == 'justify'):
+    output_lines = LineBreaker.justify_lines(output_lines)
+    
+output_text = '\n'.join(output_lines)
+
+arguments.output.write(output_text)
+arguments.output.close()
